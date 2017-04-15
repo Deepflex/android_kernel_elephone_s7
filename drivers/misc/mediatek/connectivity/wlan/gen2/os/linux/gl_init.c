59569e84c8fda4e0023d34f4c52201c6ad3f6eed
Maxim <hqdevnews@gmail.com>
2017-04-16 00:23:49 +0700

    Update gl_init.c

--- drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_init.c
+++ drivers/misc/mediatek/connectivity/wlan/gen2/os/linux/gl_init.c
@@ -855,7 +855,7 @@ const UINT_32 mtk_cipher_suites[5] = {
 /*********************************************************/
 
 #define NIC_INF_NAME    "wlan%d"	/* interface name */
-#if CFG_TC1_FEATURE
+#if CFG_TC1_FEATURE || defined(CONFIG_MTK_COMBO_AOSP_TETHERING_SUPPORT)
 #define NIC_INF_NAME_IN_AP_MODE  "legacy%d"
 #endif
 
@@ -2334,7 +2334,7 @@ static struct wireless_dev *wlanNetCreate(PVOID pvData)
 	prGlueInfo = (P_GLUE_INFO_T) wiphy_priv(prWdev->wiphy);
 	kalMemZero(prGlueInfo, sizeof(GLUE_INFO_T));
 	/* 4 <3.1> Create net device */
-#if CFG_TC1_FEATURE
+#if CFG_TC1_FEATURE || defined(CONFIG_MTK_COMBO_AOSP_TETHERING_SUPPORT)
 	if (wlan_if_changed) {
 		prGlueInfo->prDevHandler = alloc_netdev_mq(sizeof(P_GLUE_INFO_T), NIC_INF_NAME_IN_AP_MODE,
 							NET_NAME_PREDICTABLE, ether_setup, CFG_MAX_TXQ_NUM);
